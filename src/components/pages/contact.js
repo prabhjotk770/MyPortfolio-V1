dbe9e7e0eea39042cba1a738d14a3e3f5ba09cb7
Prabhjot Kaur <prabhjotk770@gmail.com>
2020-06-04 02:50:05 +0530

    contact form updated

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -49,17 +49,19 @@ const Contact = () => {
   <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
 </svg> to respond!!</h5>
       </div>
-      <form
+
+      <form method="post"
         style={{ marginLeft: "20px", marginRight: "20px" }}
       >
+        <input type="hidden" name="form-name" value="Contact" />
         <div class="form-group">
           <label for="exampleInputEmail1">Name</label>
           <input
             type="text"
-           
             class="form-control"
             id="name"
             placeholder="Enter name"
+            name="name"
           />
         </div>
 
@@ -67,11 +69,11 @@ const Contact = () => {
           <label for="exampleInputEmail1">Email address</label>
           <input
             type="email"
-           
             class="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            name="email"
           />
         </div>
 
@@ -82,10 +84,11 @@ const Contact = () => {
            
             id="exampleFormControlTextarea1"
             rows="3"
+            name="message"
           />
         </div>
 
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" class="btn btn-primary" value="send">
           Submit
         </button>
       </form>
